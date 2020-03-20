31909709faddf7bc58ca5edb247d8e6f55bd758f
Matthias Mayr <matthias.mayr@cs.lth.se>
2020-03-20 14:24:11 +0100

    Fix: Corrects add-apt-repo command.
    Previously the distribution appeared twice in the generated sources.list entry.

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -31,11 +31,13 @@ fi
 # Install realsense drivers
 # Get distribution environment variables
 . /etc/lsb-release
-export repo="http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo $DISTRIB_CODENAME main"
-if ! grep -q "^deb .*$repo" /etc/apt/sources.list /etc/apt/sources.list.d/*; then\
+export repo="http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo"
+export repo_check="$repo $DISTRIB_CODENAME main"
+export repo_add="$repo main"
+if ! grep -q "^deb .*$repo_check" /etc/apt/sources.list /etc/apt/sources.list.d/*; then\
     sudo apt-key adv --keyserver keys.gnupg.net --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
     sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
-    sudo add-apt-repository "$repo" -u
+    sudo add-apt-repository "$repo_add" -u
 else
     echo "Realsense repo exists already."
 fi
