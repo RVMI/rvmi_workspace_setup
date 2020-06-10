cf3c26e0896bd8cd33438aac821d8f479971520e
emmanuelkring <nat15ekr@student.lu.se>
2020-06-10 03:50:12 +0200

    additional fix for issue #8

--- scripts/fresh_install.sh
+++ scripts/fresh_install.sh
@@ -57,11 +57,12 @@ else
     exit -1
 fi
 
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt update
+
 #ROS install
 if check-pkg-installed ros-$ROS_DISTRO-desktop -eq 0; then
-    sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-    sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-    sudo apt update
     sudo apt install ros-$ROS_DISTRO-desktop
     install-pkg $PYTHON-rosdep
     sudo rosdep init
